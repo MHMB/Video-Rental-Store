4a90324bb7aa52a288e1e62f4163638017235e2a
mohammad hosein moti <m.h.motie@gmail.com>
2018-08-13 10:04:36 +0430

    8-18-2018 End of first section of course Exercises remained.

--- VideoMovies/App_Start/RouteConfig.cs
+++ VideoMovies/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace VideoMovies
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //following method is easier in big projects and more human readable
+            routes.MapMvcAttributeRoutes();
+            //or you can do this one but not suggested
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller= "Movies", action="ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" }
+            //    //new {year = @"2015|2016", month = @"\d{2}"}
+            //    );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ VideoMovies/Models/RandomMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoMovies.Models
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customwers { get; set; }
+    }
+}
